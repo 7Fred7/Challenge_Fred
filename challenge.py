3479c48299fa1ca7eae7067e092d1db9d136b460
7Fred7 <fredy989@gmail.com>
2021-10-20 02:06:42 -0300

    actualización challenge
    partes

--- challenge.py
+++ challenge.py
@@ -5,6 +5,7 @@ from scipy import stats
 import pandas as pd                   # PANDAS: para leer analizar datos
 import cv2                            # Libreria recomendada para imagenes
 import os      
+import matplotlib.pyplot as plt       # Libreria para graficar
 #*****************************************************************************
 # LEER RESOLUCIÓN DE IMAGENES
 input_images_path = "C:/Users/operador/Documents/Challenge_PPS/Challenge_Fred/obj"
@@ -66,3 +67,42 @@ print ('El valor minimo es: ' , minimo(resolution))
 print ('Media:', Media)
 print ('Mediana: ', Mediana)
 print ('Moda:', Moda)
+#******************************************************************************
+# PARTE 2
+# LEER TEXTOS
+input_txt_path = "C:/Users/operador/Documents/InfiniemLabs/visDrone/obj"
+files_names = os.listdir(input_txt_path)
+# print (files_names)                 # OBSERVAR SI ESTA TOMANDO LOS TXT
+clase = []                             # Almacenar en listas
+pos_X = []
+pos_Y = []
+clase_ancho = []
+clase_alto = []
+for file_name in files_names:
+      # print(file_name)
+      if file_name.split(".")[-1] not in ["txt"]:
+            continue                   #SOLO LEER TEXTOS
+      image_path = input_images_path + "/" + file_name
+      #print (image_path)              # OBSERVAR SI ESTA TOMANDO EL PATH
+      datos = pd.read_csv(image_path ,sep=' ',header=None, 
+                          names = ['Clase','Pos_X','Pos_Y','Ancho','Alto',])
+      clase.append(datos.iloc[:, 0])   # Extraer Columnas
+      pos_X.append(datos.iloc[:, 1])
+      pos_Y.append(datos.iloc[:, 2])
+      clase_ancho.append(datos.iloc[:, 3])
+      clase_alto.append(datos.iloc[:, 4])
+   
+clase_conc = np.block(clase)           # Concatena todas las listas de clases de cada dataFrame.
+max_clase = np.amax(clase_conc)        # MÁXIMO VALOR DE UNA CLASE 
+#print (clase)
+#print (clase_conc)
+#print (datos)
+
+#*****************************************************************************
+# HISTOGRAMA
+plt.hist(clase_conc, max_clase,color = 'red',ec="black") 
+plt.xlabel('Clases')                                             
+plt.ylabel('Cantidad de apariciones')                            
+plt.title('Distribución de aparición de clases en datos ingresados')
+plt.show() 
+#*****************************************************************************
